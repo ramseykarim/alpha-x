c7df9ca989b581f978ec882c30ecfa74c5102ea7
Ramsey Karim <rkarim@astro.umd.edu>
2019-05-30 16:09:26 -0400

    ready to try larger sample

--- v2/apply_chisquared.py
+++ v2/apply_chisquared.py
@@ -8,11 +8,11 @@ import pickle
 #  and alpha shape them
 sky = False
 data_dir = "/n/sgraraid/filaments/data/TEST4/helpss_scratch_work/MantiPython/"
-data_file = "points_file_smallest.pkl"
-tree_file = "atree_file_smallest.pkl"
-if False:
+data_file = "points_file_small.pkl"
+tree_file = "atree_file_small.pkl"
+if True:
     with open(data_dir + data_file, 'rb') as pfl:
-        data = pickle.load(pfl)[:1000, :]
+        data = pickle.load(pfl)
     data[:, 0] *= 4./134.
     data[:, 0] += 10
     data[:, 1] *= 3./100.
@@ -21,52 +21,54 @@ if False:
     data[:, 2] += 20
     print("DATA SHAPE:", data.shape)
     apy.initialize(data)
-    apy.KEY.alpha_step = 0.9
-    apy.KEY.orphan_tolerance = 100
+    apy.KEY.alpha_step = 0.95
+    apy.KEY.orphan_tolerance = 1000
     apy.recurse()
     with open(data_dir + tree_file, 'wb') as pfl:
         pickle.dump(apy.KEY, pfl)
+    print("Wrote tree")
     sys.exit()
 
-with open(data_dir + tree_file, 'rb') as pfl:
-    apy.reinitialize(pickle.load(pfl))
-rectangles, base_width, lim = apy.dendrogram()
-lim_alpha_lo, lim_alpha_hi = lim
-fig = plt.figure()
-d_ax, m_ax, surface_plotter = apy.prepare_plots(fig)
-for r in rectangles:
-    d_ax.add_artist(r)
-d_ax.set_xlim([-0.05 * base_width, 1.05 * base_width])
-d_ax.set_ylim([lim_alpha_lo * 0.9, lim_alpha_hi * 1.1])
-if apy.DIM == 3:
-    pass
-    # d_ax.set_xlim([175e2, 24e3])
-    # d_ax.set_ylim([0.227, 0.545])
-d_ax.set_yscale('log')
-d_ax.set_xlabel("Relative cluster size")
-d_ax.set_ylabel("Alpha")
-d_ax.set_xticklabels([])
-d_ax.invert_yaxis()
+if False:
+    with open(data_dir + tree_file, 'rb') as pfl:
+        apy.reinitialize(pickle.load(pfl))
+    rectangles, base_width, lim = apy.dendrogram()
+    lim_alpha_lo, lim_alpha_hi = lim
+    fig = plt.figure()
+    d_ax, m_ax, surface_plotter = apy.prepare_plots(fig)
+    for r in rectangles:
+        d_ax.add_artist(r)
+    d_ax.set_xlim([-0.05 * base_width, 1.05 * base_width])
+    d_ax.set_ylim([lim_alpha_lo * 0.9, lim_alpha_hi * 1.1])
+    if apy.DIM == 3:
+        pass
+        # d_ax.set_xlim([175e2, 24e3])
+        # d_ax.set_ylim([0.227, 0.545])
+    d_ax.set_yscale('log')
+    d_ax.set_xlabel("Relative cluster size")
+    d_ax.set_ylabel("Alpha")
+    d_ax.set_xticklabels([])
+    d_ax.invert_yaxis()
 
-surfaces_list, points_list = apy.naive_point_grouping()
-# surfaces_list, points_list = apy.alpha_surfaces(6.24)
-for s in surfaces_list:
-    surface_plotter(s)
-for points, color, transparency in points_list:
-    m_ax.plot(*points, marker='.', color=color, alpha=transparency, linestyle='None', markersize=1)
+    surfaces_list, points_list = apy.naive_point_grouping()
+    # surfaces_list, points_list = apy.alpha_surfaces(6.24)
+    for s in surfaces_list:
+        surface_plotter(s)
+    for points, color, transparency in points_list:
+        m_ax.plot(*points, marker='.', color=color, alpha=transparency, linestyle='None', markersize=1)
 
-if sky:
-    m_ax.set_xlabel("RA")
-    m_ax.invert_xaxis()
-    m_ax.set_ylabel("Dec")
-else:
-    m_ax.set_xlabel("X")
-    m_ax.set_ylabel("Y")
-    m_ax.set_xlim([10, 14])
-    m_ax.set_ylim([20, 22])
-    m_ax.set_aspect('equal')
-if apy.DIM == 3:
-    m_ax.set_zlabel("Z")
-    m_ax.set_zlim([20, 23])
-    # m_ax.set_zlabel("equivalent radial \"angle\" (deg)")
-plt.show()
+    if sky:
+        m_ax.set_xlabel("RA")
+        m_ax.invert_xaxis()
+        m_ax.set_ylabel("Dec")
+    else:
+        m_ax.set_xlabel("X")
+        m_ax.set_ylabel("Y")
+        m_ax.set_xlim([10, 14])
+        m_ax.set_ylim([20, 22])
+        m_ax.set_aspect('equal')
+    if apy.DIM == 3:
+        m_ax.set_zlabel("Z")
+        m_ax.set_zlim([20, 23])
+        # m_ax.set_zlabel("equivalent radial \"angle\" (deg)")
+    plt.show()
